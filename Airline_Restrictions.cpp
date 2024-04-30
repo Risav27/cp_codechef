a081f0fcf14159e10838d6d6f67483154f521f2b
Risav Kumar Jana <risavkumarjana@gmail.com>
2024-05-01 02:48:25 +0530

    new add

---
+++ Airline_Restrictions.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define vi              vector <int>
+#define vii             vector<vector<int>>
+#define vpii            vector<pair<int,int>>
+#define mii             map<int , int>
+#define rep(i,a,n)      for(int i = a; i<n; i++)
+#define pb           push_back
+#define ff          first
+#define ss          second
+
+typedef long long     ll;
+
+
+#define int    ll
+
+const int MOD  = 1e9 +7 , N = 1e5 +2 ;
+
+
+
+void solve(){
+
+
+}
+
+
+signed main(){
+
+int t = 1;
+cin>>t;
+while (t--) {
+    solve();
+
+
+}
+return 0;
+}
